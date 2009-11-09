0b99250b43f19a4e145df05b547ef74f15b24c34
James Sadler <freshtonic@gmail.com>
2009-11-09 17:27:46 +1100

    Use git clone --mirror.
    This means that we get a branch in the cloned repo for every branch
    in the remote repo, and when people clone from bignananajour, they
    will get remotes for each branch we have.

--- lib/bananajour/eater.rb
+++ lib/bananajour/eater.rb
@@ -5,16 +5,13 @@ class Bananajour::Eater
   def go!
     while true do
       @browser.repositories.each do |remote_repo|
-        repo = Bananajour::Repository.for_name remote_repo.name
         unless remote_repo.ismirror 
-          remote_name = make_remote_name(remote_repo.person.email)
-          if repo.exists?
-            link_to_remote(repo, remote_repo, remote_name) unless repo.grit_repo.remotes.select{|r| r.name =~ Regexp.new("^#{remote_name}")}.size > 0
-            fetch_remote(repo, remote_name)
+          local_repo = Bananajour::Repository.for_name(make_local_repo_name(remote_repo))
+          if local_repo.exists?
+            fetch_latest(local_repo)
           else
-            clone_repo(remote_repo, remote_repo.name)
-            link_to_remote(repo, remote_repo, remote_name) 
-            fetch_remote(repo, remote_name)
+            clone_repo(remote_repo)
+            fetch_latest(local_repo)
           end
         end
       end
@@ -22,23 +19,24 @@ class Bananajour::Eater
     end
   end
   private
-  def make_remote_name(email)
-    # Use the email address as the remote name.
-    # We need to mangle it to make sure it's a valid remote name though.
-    # NOTE: this is probably too pessimistic, and will probably piss of people with
-    # none ASCII-range characters in their email address.  Good enough for now.
-    email.gsub(/@/, "_at_").gsub(/\./, "_dot_").gsub(/[^A-Za-z0-9\._-]/, "")
-  end
-  # Creates a remote in repo that points to remote_repo.
-  # The name of the remote will be the mangled email address of the remote_repo's creator.
-  def link_to_remote(repo, remote_repo, remote_name)
-    `cd #{repo.path} && git remote add #{remote_name} #{remote_repo.uri}`
-  end
-  def fetch_remote(repo, remote_name)
+  def fetch_latest(repo)
     # Grit doesn't do fetching(!) so, we'll invoke this ourselves.
-    `cd #{repo.path} && git fetch #{remote_name}`
+    # Note: the repo's were created with git --mirror
+    # This means that when when we do git fetch, it's actually updating the BRANCHES.
+    # This is so that git clone will create remote refs in the cloned repo.
+    `cd #{repo.path} && git fetch -f`
   end
-  def clone_repo(remote_repo, local_repo_name)
-    `cd #{Bananajour.repositories_path} && git clone --bare #{remote_repo.uri} #{local_repo_name}.git`
+  def clone_repo(remote_repo)
+    # FYI: git clone --bare doesn't add the remote 'origin', need to do it manually.
+    system(["cd #{Bananajour.repositories_path}",
+    "(mkdir #{sanitize_email(remote_repo.person.email)} || true)",
+    "git clone --mirror #{remote_repo.uri} #{make_local_repo_name(remote_repo)}.git"
+    ].join(" && "))
+  end
+  def sanitize_email(email)
+    email.gsub(/@/, "_at_").gsub(/\./, "_dot_")
+  end
+  def make_local_repo_name(remote_repo)
+    sanitize_email(remote_repo.person.email) + "/" + remote_repo.name
   end
 end
